1f070c96c612f1e1a668cae764cdbad149873bde
stamovm <m.stamov@yahoo.com>
2022-06-28 23:41:13 -0700

    Tested different styles, updateed next.js

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,10 +1,17 @@
-import * as React from 'react'
 import type { NextPage } from 'next'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import HomeIcon from '@mui/icons-material/Home'
 import Link from '../src/Link'
+
+const myStyle = {
+  color: 'darkslategray',
+  backgroundColor: 'lightblue',
+  borderRadius: 4,
+  '&:hover': { backgroundColor: 'blue', color: 'yellow' },
+}
 
 const Test: NextPage = () => {
   return (
@@ -21,11 +28,26 @@ const Test: NextPage = () => {
         <Typography variant="h4" component="h1" gutterBottom>
           Test Page
         </Typography>
+        <Typography variant="h5" color="purple">
+          Testing the typography component
+        </Typography>
+        <Typography variant="h6" color="secondary">
+          Testing the typography component 2
+        </Typography>
         <Box maxWidth="sm">
-          <Button variant="contained" component={Link} noLinkStyle href="/">
+          <Button
+            variant="contained"
+            component={Link}
+            noLinkStyle
+            href="/"
+            startIcon={<HomeIcon />}
+            sx={myStyle}
+          >
             Go to the home page
           </Button>
         </Box>
+        <div>test component</div>
+        <HomeIcon color="error" />
       </Box>
     </Container>
   )
