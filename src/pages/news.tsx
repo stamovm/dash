450874b719c1a983dcb8a6f00b7f427366d888dc
stamovm <m.stamov@yahoo.com>
2022-08-07 23:14:20 -0700

    Fetch and display news

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -4,22 +4,29 @@ import { useQuery } from '@tanstack/react-query'
 
 const News: NextPage = () => {
   const { isLoading, error, data } = useQuery(['weatherData'], () =>
-    axios.get('news/api').then((res) => res.data)
+    axios
+      .get('https://inshorts.deta.dev/news?category=technology')
+      .then((res) => res.data)
   )
 
   if (isLoading) return <h1>Loading...</h1>
   if (error) return <h2>An error has occurred: {error.message}</h2>
 
-  const tempArray = data
-
+  const tempArray = data.data
+  console.log('data-: ', data)
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-4 lg:grid-cols-5">
-      {tempArray.map((temp: any) => (
-        <div
-          className="p-3 transition ease-in delay-500 card "
-          key={temp.number}
-        >
-          news card
+      {tempArray.map((news: any, i: number) => (
+        <div className="p-3 transition ease-in delay-500 card " key={i}>
+          <p className="py-2 text-teal-800">{news.date}</p>
+          <a href={news.readMoreUrl}>
+            <p className="hover:text-teal-700">{news.title}</p>
+          </a>
+          <a href={news.url}>
+            <img src={news?.imageUrl} alt={news.title}></img>
+          </a>
+          <p className="py-2">{news.content}</p>
+          <p>Author: {news.author}</p>
         </div>
       ))}
     </div>
